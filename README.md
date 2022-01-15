a2411fc077efcad8b374dd361c922dfecb3e4614
danigb <danigb@gmail.com>
2022-01-15 11:23:40 +0100

    chore: move oscillator to its own file

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 
 ## References
 
+### Synth
+
 - http://basicsynth.com/
 - https://thewolfsound.com/sound-synthesis/wavetable-synth-in-rust/
 - https://www.lulu.com/search/?keyword=synthesizers
+
+### Rust
+
+- https://github.com/mre/idiomatic-rust
+- https://doc.rust-lang.org/rust-by-example/mod/split.html
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,5 @@
-use core::time::Duration;
+mod wavetable_oscillator;
 use rodio::{source::Source, OutputStream};
-
-struct WavetableOscillator {
-    sample_rate: u32,
-    wave_table: Vec<f32>,
-    index: f32,
-    index_increment: f32,
-}
-
-impl WavetableOscillator {
-    fn new(sample_rate: u32, wave_table: Vec<f32>) -> WavetableOscillator {
-        return WavetableOscillator {
-            sample_rate,
-            wave_table,
-            index: 0.0,
-            index_increment: 0.0,
-        };
-    }
-
-    fn set_frequency(&mut self, frequency: f32) {
-        self.index_increment = frequency * self.wave_table.len() as f32 / self.sample_rate as f32;
-    }
-
-    fn get_sample(&mut self) -> f32 {
-        let sample = self.lerp();
-        self.index += self.index_increment;
-        self.index %= self.wave_table.len() as f32;
-        return sample;
-    }
-
-    fn lerp(&self) -> f32 {
-        let truncated_index = self.index as usize;
-        let next_index = (truncated_index + 1) % self.wave_table.len();
-        let next_index_weight = self.index - truncated_index as f32;
-        let truncated_index_weight = 1.0 - next_index_weight;
-
-        return truncated_index_weight * self.wave_table[truncated_index]
-            + next_index_weight * self.wave_table[next_index];
-    }
-}
-
-impl Iterator for WavetableOscillator {
-    type Item = f32;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        Some(self.get_sample())
-    }
-}
-
-impl Source for WavetableOscillator {
-    fn channels(&self) -> u16 {
-        1
-    }
-
-    fn sample_rate(&self) -> u32 {
-        self.sample_rate
-    }
-
-    fn current_frame_len(&self) -> Option<usize> {
-        None
-    }
-
-    fn total_duration(&self) -> Option<Duration> {
-        None
-    }
-}
 
 fn main() {
     let wave_table_size = 64;
@@ -73,7 +8,7 @@ fn main() {
         wave_table.push((2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin());
     }
 
-    let mut oscillator = WavetableOscillator::new(44100, wave_table);
+    let mut oscillator = wavetable_oscillator::WavetableOscillator::new(44100, wave_table);
     oscillator.set_frequency(440.0);
 
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
