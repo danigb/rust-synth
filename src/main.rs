4b4ae07230a6e409cb6e89d0ac219d7442bf8245
danigb <danigb@gmail.com>
2022-01-15 16:48:38 +0100

    chore: matches the implementation of BS 5.8

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,20 @@ use hound;
 use rodio::{source::Source, OutputStream};
 
 fn main() {
+    let sample_rate = 44100;
     let spec = hound::WavSpec {
+        sample_rate,
         channels: 1,
-        sample_rate: 44100,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
     let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
-    for t in (0..44100).map(|x| x as f32 / 44100.0) {
-        let sample = (t * 440.0 * 2.0 * PI).sin();
+    let angular_freq = 2 as f32 * PI / sample_rate as f32;
+    let duration_in_seconds = 2.0;
+    let total_samples = (sample_rate as f32 * duration_in_seconds) as u32;
+    let freq = 440.0;
+    for n in 0..total_samples {
+        let sample = (n as f32 * freq * angular_freq).sin();
         let amplitude = i16::MAX as f32;
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
