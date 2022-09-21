b20f19e17a83e91cfc10d9c8d316df4f384405e2
danigb <danigb@gmail.com>
2022-09-21 22:34:33 +0200

    feat: connect Envelope to Gain

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+mod amp;
 mod buffer;
-mod env;
-mod osc;
+mod envelope;
 mod phasor;
+mod signal;
 mod wavetable;
+mod wavetable_oscillator;
 
-use osc::WavetableOscillator;
+use amp::Amp;
 use phasor::Phasor;
+use signal::{Constant, Impulse, Signal};
+use wavetable_oscillator::WavetableOscillator;
 
 const SAMPLE_RATE: u32 = 44100;
 const WAVETABLE_SIZE: usize = 1024;
 
 fn main() {
-    let mut phasor = Phasor::new(SAMPLE_RATE);
-    phasor.set_frequency(440.0);
+    let mut phasor = Phasor::new(SAMPLE_RATE, Constant::new(440.0));
     let mut buffer = buffer::create_buffer(SAMPLE_RATE, 2.0);
     for _ in 0..buffer.capacity() {
         buffer.push(phasor.tick());
@@ -20,23 +23,23 @@ fn main() {
     buffer::write_buffer("phasor.wav", SAMPLE_RATE, buffer);
 
     let sin_wt = wavetable::sin(WAVETABLE_SIZE);
-    let mut osc = WavetableOscillator::new(SAMPLE_RATE, sin_wt);
-    osc.set_frequency(440.0);
+    let mut osc = WavetableOscillator::new(SAMPLE_RATE, sin_wt, Constant::new(440.0));
     let mut buffer = buffer::create_buffer(SAMPLE_RATE, 2.0);
     for _ in 0..buffer.capacity() {
         buffer.push(osc.tick());
     }
     buffer::write_buffer("osc.wav", SAMPLE_RATE, buffer);
 
-    let mut env = env::Envelope::new(SAMPLE_RATE);
+    let mut env = envelope::Envelope::new(SAMPLE_RATE, Impulse::new());
     env.set_attack(0.1);
     env.set_release(0.2);
     env.set_hold_in_seconds(0.1);
 
+    let mut amp = Amp::new(osc, env);
+
     let mut buffer = buffer::create_buffer(SAMPLE_RATE, 2.0);
-    buffer.push(env.tick(1.0));
-    for _ in 1..buffer.capacity() {
-        buffer.push(env.tick(0.0))
+    for _ in 0..buffer.capacity() {
+        buffer.push(amp.tick())
     }
-    buffer::write_buffer("env.wav", SAMPLE_RATE, buffer);
+    buffer::write_buffer("result.wav", SAMPLE_RATE, buffer);
 }
